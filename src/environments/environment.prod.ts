e5bec70216df71d40703a958d6e80481e264c893
Erick Garcia <erick.adan@gmail.com>
2022-10-15 20:19:46 -0600

    Final Version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //api_url : 'http://34.71.179.252:3000/api'
-  api_url : 'http://localhost:3000/api'
+  api_url : 'http://34.123.36.80:3000/api'
+  //api_url : 'http://localhost:3000/api'
 };
