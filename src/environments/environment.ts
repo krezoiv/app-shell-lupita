25e31f735991abc1257f0ac62f4a50e3f0b53352
krezoiv <erick.adan@gmail.com>
2022-10-18 01:35:30 -0600

    18.10

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
- // api_url : 'http://34.123.36.80:3000/api'
-  api_url: 'http://localhost:3000/api'
+ api_url : 'http://34.123.36.80:3000/api'
+  //api_url: 'http://localhost:3000/api'
 };
 
 /*
