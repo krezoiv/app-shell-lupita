cc2810999827c6eec06e87d61a60f1c80bba84ba
krezoiv <erick.adan@gmail.com>
2022-11-17 17:59:14 -0600

    17.11

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+//api url por production
 export const environment = {
   production: false,
  api_url : 'http://34.136.57.34:3000/api'
